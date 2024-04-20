c8a5c47f60cafaef34431931acbbb7fe4058b697
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-20 18:01:31 +0000

    datatypes done

--- JavaScript/2.DataTypes.js
+++ JavaScript/2.DataTypes.js
@@ -1,3 +1,10 @@
+// 2 types od data on basis of how they store in memory
+//premitive => string,boolean,number,bigint,symbol,undefined,null
+//reference (non premitive) => object , function , array
+
+//datatype of null is always object
+//non premitive data types have type objet , fucntion has types function-object
+
 let fname="Mamoona"
 let age=20
 let IsLoggedIn=true
