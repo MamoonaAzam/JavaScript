76d6e2e43c643a7af765dc31b063511bc7516311
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-20 13:57:32 +0000

    conatiner config done

---
+++ JavaScript/3.TypeConversion.js
@@ -0,0 +1,16 @@
+let value1=20
+let value2="20"
+let value3="30mamaoona"
+console.log(typeof value1);
+console.log(typeof value2);
+
+//Converting string value to number
+
+let convetredValue1= Number(value2)
+console.log(typeof convetredValue1); 
+
+//value=30mamoona so how it can be converted
+let convetredValue2 = Number(value3)
+console.log(typeof convetredValue2);
+console.log(convetredValue2)
+//its giving NaN which says its not a number
