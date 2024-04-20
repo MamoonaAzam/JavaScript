41de0145badfa384dff048e438aea7a44ebc1636
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-20 14:07:38 +0000

    container config done

--- JavaScript/3.TypeConversion.js
+++ JavaScript/3.TypeConversion.js
@@ -1,6 +1,11 @@
+// conclusion
+//"33" = 33 easily
+//"33abc" = NAN
+//"Null" = 0
+//"Undefined" = NaN
+
 let value1=20
 let value2="20"
-let value3="30mamaoona"
 console.log(typeof value1);
 console.log(typeof value2);
 
@@ -10,7 +15,26 @@ let convetredValue1= Number(value2)
 console.log(typeof convetredValue1); 
 
 //value=30mamoona so how it can be converted
+let value3="30mamaoona"
 let convetredValue2 = Number(value3)
 console.log(typeof convetredValue2);
 console.log(convetredValue2)
 //its giving NaN which says its not a number
+
+
+let score=null
+console.log(typeof score)
+//lets convert it into number
+let convertedScore=Number(score)
+console.log(typeof convertedScore)
+console.log(convertedScore)
+
+//convering into boolean
+let number=1
+let booleanvalue=Boolean(number)
+console.log(booleanvalue)
+//cocnlusion in boolen 
+//True =1
+//False =0
+// ""= false
+// "mamoona" = true
