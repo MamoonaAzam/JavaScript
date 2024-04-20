56bc32dae3372b6785cd3a624eddb00c47b90466
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-20 13:18:06 +0000

    container config done

---
+++ JavaScript/1.Variables.js
@@ -0,0 +1,17 @@
+// conclusion 
+// const value cant be changed
+// its better to use let than var
+
+const fname="Mamoona"
+let age=23
+var city="Bwp"
+education="Bscs"
+
+//now lets change there values to see what happens
+
+// fname="ayesha"        -> doing this is not allowed
+age=30
+city="Multan"
+education="Inter"
+
+console.table([fname,age,city,education])
