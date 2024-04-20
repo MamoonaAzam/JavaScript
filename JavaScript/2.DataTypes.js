4b35c7747870338fdbccf36b5518050b2b72197c
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-20 13:38:15 +0000

    container config done

---
+++ JavaScript/2.DataTypes.js
@@ -0,0 +1,11 @@
+let fname="Mamoona"
+let age=20
+let IsLoggedIn=true
+let city ;  //value is undefined
+let education =null //It is stand alone value , refers empty , its an object type
+
+console.log(typeof(fname))
+console.log(typeof(age))
+console.log(typeof(IsLoggedIn))
+console.log(typeof(city))
+console.log(typeof(education))
