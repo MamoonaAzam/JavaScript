a0975ae024e031fcefdc82c1b487ab2b74d6181c
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-21 06:52:58 +0000

    string done

--- JavaScript/6.Strings.js
+++ JavaScript/6.Strings.js
@@ -1,5 +1,23 @@
 let stringvalue="Hi how are you!"
 console.log(typeof stringvalue)
 
+//2nd way of string declaration
 let stringvalue2= new String("Step by Step") //it will give object type
 console.log(typeof stringvalue2)
+console.log(stringvalue2.substring(0,4))
+
+let fname="ali"
+let lname="ahmed"
+
+// concatination
+let fullName=(fname + lname)
+console.log(fullName)
+
+//string interpolation 
+console.log(`Your fIrst name is ${fname} Your last name is ${lname}`)
+
+ //String functions:
+ console.log(`Your fIrst name is ${fname.toUpperCase()} Your last name is ${lname}`)
+ console.log(`Your fIrst name is ${fname.charAt(1)} Your last name is ${lname.charAt(2)}`)
+ console.log(`Your fIrst name length is ${fname.length} Your last name length is ${lname.length}`)
+ console.log(`Your fIrst name is ${fname.indexOf("ali")} Your last name is ${lname.indexOf("h")}`)
