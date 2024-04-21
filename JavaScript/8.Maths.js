6d8a0b4042ec3a7ee61bdd7a9f30278b88a053ba
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-21 12:07:53 +0000

    maths done

--- JavaScript/8.Maths.js
+++ JavaScript/8.Maths.js
@@ -1,4 +1,5 @@
 
+let val=20.94
 let x=40
 let y=10.73
 let z=-10
@@ -6,6 +7,18 @@ let z=-10
 //console.log(Math.round(y)) // round of any value
 console.log(`round of value of y=${y} is ${Math.round(y)}`) 
 
+// if there is anything above absolute it will consider it next number
+console.log(`ceil of value of val=${val} is ${Math.ceil(val)}`) 
+
+// if there is anything above absolute it will take it down
+console.log(`floor of value of val=${val} is ${Math.floor(val)}`) 
+
 //console.log(Math.abs(z)) // provide absolute value meansing convert -ve -> +ve only
 console.log(`absolute of value of z=${z} is ${Math.abs(z)}`)
+
+
+const min=100
+const max=200
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+
 
