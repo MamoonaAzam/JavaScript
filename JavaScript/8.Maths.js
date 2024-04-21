11957703d6d78c57efc68b4ff8bde0084eec8c28
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-21 11:23:52 +0000

    maths done

---
+++ JavaScript/8.Maths.js
@@ -0,0 +1,11 @@
+
+let x=40
+let y=10.73
+let z=-10
+
+//console.log(Math.round(y)) // round of any value
+console.log(`round of value of y=${y} is ${Math.round(y)}`) 
+
+//console.log(Math.abs(z)) // provide absolute value meansing convert -ve -> +ve only
+console.log(`absolute of value of z=${z} is ${Math.abs(z)}`)
+
