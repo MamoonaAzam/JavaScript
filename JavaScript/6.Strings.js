4181e2bb98082749f37bc928e35957f3db8da7d2
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-21 07:11:04 +0000

    strings done

--- JavaScript/6.Strings.js
+++ JavaScript/6.Strings.js
@@ -5,6 +5,7 @@ console.log(typeof stringvalue)
 let stringvalue2= new String("Step by Step") //it will give object type
 console.log(typeof stringvalue2)
 console.log(stringvalue2.substring(0,4))
+console.log(stringvalue2.slice(-12,7))
 
 let fname="ali"
 let lname="ahmed"
@@ -21,3 +22,14 @@ console.log(`Your fIrst name is ${fname} Your last name is ${lname}`)
  console.log(`Your fIrst name is ${fname.charAt(1)} Your last name is ${lname.charAt(2)}`)
  console.log(`Your fIrst name length is ${fname.length} Your last name length is ${lname.length}`)
  console.log(`Your fIrst name is ${fname.indexOf("ali")} Your last name is ${lname.indexOf("h")}`)
+
+ //trim and replace
+ let email="   example@gmail.com"
+ console.log(email)
+ console.log(email.trim())
+ console.log(email.replace('example','mamona'))
+
+ let randomvalue="Hi-How-are-U"
+ console.log(randomvalue.split("-"))
+ console.log(randomvalue.includes("How"))
+ console.log(randomvalue.includes("how"))
