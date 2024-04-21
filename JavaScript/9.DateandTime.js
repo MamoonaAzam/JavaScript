a0201376d5b910ad518a23ee54b9ccb6ea066b84
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-21 12:43:28 +0000

    datetime

---
+++ JavaScript/9.DateandTime.js
@@ -0,0 +1,6 @@
+let mydate= new Date()
+console.log(typeof mydate)
+console.log(mydate) //difficult to read
+console.log(mydate.toDateString())
+console.log(mydate.toLocaleString())
+console.log(mydate.toLocaleDateString())
