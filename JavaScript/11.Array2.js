623ca80cfc06cab8b96c5337e89396c37e8808fd
Mamoona Azam <113453051+MamoonaAzam@users.noreply.github.com>
2024-04-22 04:10:07 +0000

    Array2 done

--- JavaScript/11.Array2.js
+++ JavaScript/11.Array2.js
@@ -2,14 +2,17 @@ const Male=["Ali","Ahmed","Adeel","Abdullah"]
 const Female=["Ayesha","Shae Gill","Laiba","Gennifer"]
 
 //concatinating array ->not recommended bcz it concatenate 2 arrays
+console.log("concatinating array")
 const AllGenders=Male.concat(Female)
 console.log(AllGenders) //provide us a new array
 
 // using spread ->mostly used for concatination
+console.log("Using spread")
 const Genders=[...Male,...Female]
 console.log(Genders)
 
 //What if we push one array to another array
+console.log("Pushing array to another array")
 Male.push(Female)
 console.log(Male) //it changes male array
 console.log(Male [4][1] .toUpperCase()) // returns SHAE Gill
